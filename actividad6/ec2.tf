bfa68ff11683824419b3ab30ebd3fdef56a55aea
afak <djem12032004@gmail.com>
2025-09-23 17:01:51 -0600

    Act6

---
+++ actividad6/ec2.tf
@@ -0,0 +1,17 @@
+locals {
+    instance_count = 2
+    instance_name = act6-Ismael_dev1-David_dev2
+}
+
+resource "aws_instance" "example" {
+  count         = local.instance_count
+  ami           = "ami-0f70b01eb0d5c5caa" 
+  instance_type = var.instance_type
+  tags = {
+    Name = "${local.instance_name}-${count.index + 1}"
+  }
+}
+
+output "server_names" {
+  value = aws_instance.ismaeldavid_server_terr_tags.Name
+}
